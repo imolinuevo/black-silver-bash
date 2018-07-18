98813ad802f2a8b0da079cbf73c6a332b4b2035e
imolinuevo <i.m.parron@opendeusto.es>
2018-07-18 22:29:14 +0200

    add initial parametrization

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# black-silver-bash
+# Black Silver 
+This is a bash script for automatic PDF form field replacing.
+
+## Args
+
+1) **user:** single word username
+2) **template:** full template url & filename without extension (must be *PDF*)
+3) **output:** full output file location url & filename without extension (will be *PDF*)
+4) **form_fields** serialized string containing the form fields key-values
+
+## Example
+
+```
+bash replace_pdf.sh admin invoice_template out/invoice
+```
--- replace_pdf.sh
+++ replace_pdf.sh
@@ -1,8 +1,10 @@
 declare -A form_fields
+timestamp=$(date +%s)
 form_fields=(["FECHA"]="test")
-pdftk invoice_template.pdf generate_fdf output invoice_tmp.fdf
+pdftk $2.pdf generate_fdf output ${timestamp}_$1_tmp.fdf
 for form_field in "${!form_fields[@]}";
-    do cat invoice_tmp.fdf | tr '\n' '\r' | sed "s/()\r\/T ($form_field)/(${form_fields[$form_field]})\r\/T ($form_field)/g" | tr '\r' '\n' > invoice.fdf;
+    do cat ${timestamp}_$1_tmp.fdf | tr '\n' '\r' | sed "s/()\r\/T ($form_field)/(${form_fields[$form_field]})\r\/T ($form_field)/g" | tr '\r' '\n' > ${timestamp}_$1.fdf;
 done
-rm invoice_tmp.fdf
-pdftk invoice_template.pdf fill_form invoice.fdf output filled.pdf
+rm ${timestamp}_$1_tmp.fdf
+pdftk $2.pdf fill_form ${timestamp}_$1.fdf output $3_${timestamp}_$1.pdf
+rm ${timestamp}_$1.fdf
