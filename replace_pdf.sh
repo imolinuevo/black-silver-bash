4fead830bde4255d92e8230cb44cdffc4adff64f
imolinuevo <i.m.parron@opendeusto.es>
2018-07-19 00:00:09 +0200

    split input into associative array

--- replace_pdf.sh
+++ replace_pdf.sh
@@ -1,6 +1,15 @@
 declare -A form_fields
+
+arr=( $4 )
+for i in "${arr[@]}";
+    do
+        key=$(echo $i | cut -f1 -d-)
+        value=$(echo $i | cut -f2 -d-)
+        form_fields["$key"]="$value"
+        echo $key $value
+done
+
 timestamp=$(date +%s)
-form_fields=(["FECHA"]="test")
 pdftk $2.pdf generate_fdf output ${timestamp}_$1_tmp.fdf
 for form_field in "${!form_fields[@]}";
     do cat ${timestamp}_$1_tmp.fdf | tr '\n' '\r' | sed "s/()\r\/T ($form_field)/(${form_fields[$form_field]})\r\/T ($form_field)/g" | tr '\r' '\n' > ${timestamp}_$1.fdf;
